5ac6f30d482fe72fe89073f5d21941e9b49fff25
kirbysamus <kirbysamus@gmail.com>
2023-11-23 13:00:47 -0600

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,90 +1,3 @@
-<style lang="scss">
-  @media (max-width: 767px) {
-#app {
-  position: relative;
-  height: 100vh;
-  overflow: auto;
-  z-index: 9999;
-}
-
-.clipped-x-large-forward {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 120%;
-  height: 136px;
-   display: flex;
-  flex-direction: column; 
-  z-index: 9999;
-}
-  clipped-x-large-forward {
-    clip-path: polygon(100% 0,100% calc(100% - 96px),calc(100% - 96px) 100%,0 100%,0 0);
-    -webkit-clip-path: polygon(100% 0,99% calc(100% - 96px),calc(101% - 91px) 100%,0 100%,0 0);
-  }
-.title clipped-x-large-forward {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 20%;
- 
-  z-index: 9999;
-  }
-  
-  .title {
-  position: relative;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 40%;
-  flex-direction: row;
-    column-gap: 1em;
-  z-index: 9999;
-  }
-  .location-info {
-  position: fixed;
-  top: 56px; /* Adjust the top position as needed */
-  left: 62%;
-  transform: translateX(-50%);
-  z-index: 99999; /* Adjust the z-index value as needed */
-  }
-  .location-row h4 {
-    margin: 0;
-    line-height: 0.5em;
-    font-family: "Raleway",sans-serif;
-    font-size: 6px;
-    font-weight: 800;
-    text-transform: uppercase;
-    letter-spacing: 3px;
-    color: #000000de
-}
-
-.location-row .subtitle {
-    font-size: 15px;
-    margin: 0%;
-    align-self: center;
-    font-family: "Big Shoulders Display",cursive;
-    font-weight: 800;
-    text-transform: uppercase;
-    letter-spacing: .1em;
-    color: #000000de
-}
-  .rhombus {
-    z-index: -1;
-    transform: translate(-5000%) skew(-.785398rad);
-    animation: .75s ease-out 1 slideInFromLeftRhombus;
-    animation-delay: 1s;
-    animation-fill-mode: forwards;
-    background-color: #991e2a;
-    border-color: #991e2a;
-    width: 0px;
-    height: 100%;
-    display: inline-flex;
-    margin-left: 308px;
-    margin-top: -23px;
-}
-  }
-</style>
 <template>
   <header class="clipped-x-large-forward">
     <div class="title clipped-x-large-forward">
@@ -147,3 +60,6 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+</style>
