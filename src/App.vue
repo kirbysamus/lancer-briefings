8aaa9c625136fc1e863a005536e3e8724489b2fd
kirbysamus <kirbysamus@gmail.com>
2023-11-23 13:15:31 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,74 @@
 <template>
-  <Header :header="this.header" />
-  <div class="content-container">
-    <section class="section-container" id="missions" style="missionSectionStyle">
-      <div class="section-header clipped-medium-backward">
-        <img src="/icons/mission-icon.svg" />
-        <h1>Mission Log</h1>
+  <div id="app">
+    <Header :header="header" />
+    <v-container>
+      <div class="content-container">
+            <section class="section-container" id="missions" :style="missionSectionStyle">
+              <div class="section-header clipped-medium-backward">
+                <img src="/icons/mission-icon.svg" />
+                <h1>Mission Log</h1>
+              </div>
+              <div class="section-content-container">
+                <h3>Current Assignment</h3>
+                <Markdown :source="current_md" class="markdown" />
+                <h3>Mission List</h3>
+                <div class="mission-list-container">
+                  <Mission
+                    v-for="item in this.missions"
+                    :key="item.slug"
+                    :mission="item"
+                    :selected="this.mission_slug"
+                    @click="selectMission(item)"
+                  />
+                </div>
+              </div>
+            </section>
+     
+            <section class="section-container" id="events" :style="eventsSectionStyle">
+              <div class="section-header clipped-medium-backward">
+                <img src="/icons/events-icon.svg" />
+                <h1>Events Log</h1>
+              </div>
+              <div class="section-content-container">
+                <Markdown :source="events" class="markdown" />
+              </div>
+            </section>
+         
+            <section class="section-container" id="pilots" :style="pilotsSectionStyle">
+              <div style="height:52px; overflow:hidden;">
+                <div class="section-header clipped-medium-backward-pilot">
+                  <img src="/icons/pilot-icon.svg" />
+                  <h1>Pilot Roster</h1>
+                </div>
+                <div class="rhombus-back">&nbsp;</div>
+              </div>
+              <div class="section-content-container">
+                <div class="pilot-list-container">
+                  <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
+                </div>
+              </div>
+            </section>
       </div>
-      <div class="section-content-container">
-        <h3>Current Assignment</h3>
-        <Markdown :source="current_md" class="markdown" />
-        <h3>Mission List</h3>
-        <div class="mission-list-container">
-          <Mission
-            v-for="item in this.missions"
-            :key="item.slug"
-            :mission="item"
-            :selected="this.mission_slug"
-            @click="selectMission(item)"
-          />
-        </div>
-      </div>
-    </section>
-    <section class="section-container" id="events" style="eventsSectionStyle">
-      <div class="section-header clipped-medium-backward">
-        <img src="/icons/events-icon.svg" />
-        <h1>Events Log</h1>
-      </div>
-      <div class="section-content-container">
-        <Markdown :source="events" class="markdown" />
-      </div>
-    </section>
-    <section class="section-container" id="pilots" style="pilotSectionStyle">
-      <div style="height:52px; overflow:hidden;">
-        <div class="section-header clipped-medium-backward-pilot">
-          <img src="/icons/pilot-icon.svg" />
-          <h1>Pilot Roster</h1>
-        </div>
-        <div class="rhombus-back">&nbsp;</div>
-      </div>
-      <div class="section-content-container">
-        <div class="pilot-list-container">
-          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
-        </div>
-      </div>
-    </section>
+      <svg style="visibility: hidden; position: relative;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="round">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
+      <audio autoplay>
+        <source src="/startup.ogg" type="audio/ogg" />
+      </audio>
+    </v-container>
+    <Footer/>
   </div>
-  <svg
-    style="visibility: hidden; position: relative;"
-    width="0"
-    height="0"
-    xmlns="http://www.w3.org/2000/svg"
-    version="1.1"
-  >
-    <defs>
-      <filter id="round">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
-        <feColorMatrix
-          in="blur"
-          mode="matrix"
-          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
-          result="goo"
-        />
-        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
-      </filter>
-    </defs>
-  </svg>
-  <audio autoplay>
-    <source src="/startup.ogg" type="audio/ogg" />
-  </audio>
-  <Footer/>
 </template>
 
 <script>
