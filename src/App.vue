1a4566fb5b72364442b0e77337a2bdc7b6d48491
kirbysamus <kirbysamus@gmail.com>
2024-02-07 23:30:09 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -89,9 +89,9 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
+      "mission_slug": "002",
       "current_md": "",
-      "events": "002.md",
+      "events": "",
       "missions": [
         {
           "slug": "001",
